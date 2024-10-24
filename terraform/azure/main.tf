1407f5f2b158305ad0a5256e496e6fdb404654c3
Williamfiap <rm556336@fiap.com.br>
2024-10-23 21:34:50 -0300

    teste deploy aws e azure com peering 3

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-    name     = "rg-staticsite-lb-multicloud-tf-williamiaciac"
+    name     = "rg-staticsite-lb-multicloud-tf-williamiaciac-rgterra"
     location = "brazilsouth"
 }
 
